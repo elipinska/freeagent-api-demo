f3d660a057b120e66ba8744057b1a3c3a5d0f1b3
Ewa Lipinska <ewa.lipinska@hotmail.com>
2020-04-16 23:48:21 +0100

    Add user info to show page

--- app/controllers/freeagent_api/authentications_controller.rb
+++ app/controllers/freeagent_api/authentications_controller.rb
@@ -11,6 +11,11 @@ module FreeagentApi
         "Authorization": "Bearer #{@freeagent_api_authentication.access_token}"
         }).body
       @company = JSON.parse(company_json)["company"]
+
+      user_json = HTTParty.get("https://api.sandbox.freeagent.com//v2/users/me", headers: {
+        "Authorization": "Bearer #{@freeagent_api_authentication.access_token}"
+        }).body
+      @user = JSON.parse(user_json)["user"]
     end
 
     # GET /freeagent_api/authentications/new
