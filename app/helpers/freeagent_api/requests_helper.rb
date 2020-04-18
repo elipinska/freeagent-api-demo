6059bf5abbe66e064809747e362f2042fe895ce7
Ewa Lipinska <ewa.lipinska@hotmail.com>
2020-04-18 03:06:18 +0100

    Improve results table format when there are multiple nested elements

--- app/helpers/freeagent_api/requests_helper.rb
+++ app/helpers/freeagent_api/requests_helper.rb
@@ -1,19 +1,17 @@
 module FreeagentApi::RequestsHelper
-  def display_nested_results(table_contents, api_response)
-      if api_response.respond_to?(:keys)
-        api_response.each do |field, value|
-          if value.respond_to?(:keys)
-            display_nested_results(table_contents, value)
-          elsif value.is_a?(Array)
-            value.each_with_index do |v, i|
-              table_contents << "<tr><td><b>#{field.singularize.capitalize} #{i + 1}</b></td><td></td></tr>"
-              display_nested_results(table_contents, v)
-            end
-          else
-            table_contents << "<tr><td>#{field.capitalize}</td><td>#{value}</td></tr>"
-          end
+  def display_nested_results(table_contents, results)
+    results.each do |field, value|
+      if value.respond_to?(:keys)
+        display_nested_results(table_contents, value)
+      elsif value.respond_to?(:first) && value.first.respond_to?(:keys)
+        value.each_with_index do |element, index|
+          table_contents << "<tr><td></td><td></td></tr>"
+          display_nested_results(table_contents, element)
         end
+      else
+        table_contents << "<tr><td>#{field.capitalize}</td><td>#{value}</td></tr>"
       end
+    end
     table_contents.html_safe
   end
 
