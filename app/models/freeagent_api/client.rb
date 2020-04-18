c742e526088aa40a5414b0154ea98dbdfeec853b
Ewa Lipinska <ewa.lipinska@hotmail.com>
2020-04-18 21:57:44 +0100

    Refactor API client to remove duplication

--- app/models/freeagent_api/client.rb
+++ app/models/freeagent_api/client.rb
@@ -5,39 +5,29 @@ module FreeagentApi
   class Client
     attr_accessor :client_id, :secret, :redirect_uri, :base_uri, :token_uri
 
-    def initialize
-      @client_id = Rails.configuration.oauth[:client_id]
-      @client_secret = Rails.configuration.oauth[:secret]
-      @redirect_uri = Rails.configuration.oauth[:redirect_uri]
-      @base_uri = Rails.configuration.oauth[:base_uri]
-      @token_uri = Rails.configuration.oauth[:token_uri]
-    end
+    CLIENT_ID = Rails.configuration.oauth[:client_id]
+    CLIENT_SECRET = Rails.configuration.oauth[:secret]
+    REDIRECT_URI = Rails.configuration.oauth[:redirect_uri]
+    BASE_URI = Rails.configuration.oauth[:base_uri]
+    TOKEN_URI = Rails.configuration.oauth[:token_uri]
 
     def authorize_url
-      URI("#{@base_uri}redirect_uri=#{@redirect_uri}&response_type=code&client_id=#{@client_id}")
+      URI("#{BASE_URI}redirect_uri=#{REDIRECT_URI}&response_type=code&client_id=#{CLIENT_ID}")
     end
 
     def request_access_token(authorisation_code:)
-      uri = Rails.configuration.oauth[:token_uri]
-      basic_auth = {
-        username: Rails.configuration.oauth[:client_id],
-        password: Rails.configuration.oauth[:secret],
-      }
+      uri = TOKEN_URI
       body = {
         grant_type: "authorization_code",
         code: authorisation_code,
-        redirect_uri: Rails.configuration.oauth[:redirect_uri],
+        redirect_uri: REDIRECT_URI,
       }
 
       HTTParty.post(uri, basic_auth: basic_auth, body: body)
     end
 
     def refresh_access_token(refresh_token:)
-      uri = Rails.configuration.oauth[:token_uri]
-      basic_auth = {
-        username: Rails.configuration.oauth[:client_id],
-        password: Rails.configuration.oauth[:secret],
-      }
+      uri = TOKEN_URI
       body = {
         grant_type: "refresh_token",
         refresh_token: refresh_token,
@@ -60,6 +50,15 @@ module FreeagentApi
     def parsed_body(message:)
       body = message.body
       JSON.parse(body) if body.present?
+    end
+
+    private
+
+    def basic_auth
+      {
+        username: CLIENT_ID,
+        password: CLIENT_SECRET,
+      }
     end
   end
 end
